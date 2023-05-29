9ac69251dd48a381f219a68e486bd8a8a396641c
XII <jacopo.cartabmb@gmail.com>
2023-05-29 14:52:53 +0200

    x

--- appunti uni/statistica/test.js
+++ appunti uni/statistica/test.js
@@ -1,6 +1,6 @@
 const dataset = [1, 3, 5, 6, 7, 7, 9, 9, 10, 11]
 const fixed_position = 4;
-
+var mean;
 function calculateMean(data) {
     const n = data.length;
     let sum = 0;
@@ -8,14 +8,13 @@ function calculateMean(data) {
     for (let i = 0; i < n; i++) {
       sum += data[i];
     }
-  
-    const mean = sum / n;
+	console.log(`Passaggi per la risoluzione: (${sum}) / ${n}`)
+    mean = sum / n;
     return mean.toFixed(fixed_position);
 }
 
 function calculateStandardDeviation(data) {
     const n = data.length;
-    const mean = calculateMean(data);
     let sumSquaredDifferences = 0;
   
     for (let i = 0; i < n; i++) {
@@ -30,7 +29,6 @@ function calculateStandardDeviation(data) {
 
 function calculateSampleVariance(data) {
     const n = data.length;
-    const mean = calculateMean(data);
     let sumSquaredDifferences = 0;
   
     for (let i = 0; i < n; i++) {
@@ -42,8 +40,8 @@ function calculateSampleVariance(data) {
     return sampleVariance.toFixed(fixed_position);
 }
 
-console.log(`Media campionaria: ${calculateMean(dataset)}
-Dev. st: ${calculateStandardDeviation(dataset)}
-S^2: ${calculateSampleVariance(dataset)}
+console.log(`- Media campionaria: ${calculateMean(dataset)} \n
+Dev. st: ${calculateStandardDeviation(dataset)} \n
+S^2: ${calculateSampleVariance(dataset)} \n
 S: ${Math.sqrt(calculateSampleVariance(dataset)).toFixed(fixed_position)}
 `)
